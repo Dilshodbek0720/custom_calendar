1dfe44d76d61e8e14d2a45185c9d86dcfe3ef462
Dilshodbek0720 <sayitqulovdilshodbek@gmail.com>
2023-10-24 18:41:59 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   DateTime today = DateTime.now();
-  void _onDaySelected(DateTime day, DateTime focusedDay){
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
     setState(() {
       today = day;
     });
@@ -24,23 +24,22 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text("Calendar"),
-            centerTitle: true,
-          ),
-          body: Column(
-            children: [
-              TableCalendar(
-                focusedDay: today,
-                locale: "en_Us",
-                // rowHeight: 48,
-                headerStyle: const HeaderStyle(
+        appBar: AppBar(
+          title: const Text("Calendar"),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            TableCalendar(
+              focusedDay: today,
+              locale: "en_Us",
+              // rowHeight: 48,
+              headerStyle: const HeaderStyle(
                   formatButtonVisible: false,
                   titleCentered: true,
                   // headerMargin: EdgeInsets.symmetric(horizontal: 6),
-                  formatButtonShowsNext: true
-                ),
-                calendarStyle: const CalendarStyle(
+                  formatButtonShowsNext: true),
+              calendarStyle: const CalendarStyle(
                   withinRangeTextStyle: TextStyle(color: Colors.red),
                   weekNumberTextStyle: TextStyle(color: Colors.red),
                   // weekendTextStyle: TextStyle(color: Colors.red,),
@@ -48,17 +47,17 @@ class _MainAppState extends State<MainApp> {
                   rangeStartTextStyle: TextStyle(color: Colors.red),
                   rangeEndTextStyle: TextStyle(color: Colors.red),
                   todayTextStyle: TextStyle(color: Colors.white),
-                  holidayTextStyle: TextStyle(color: Colors.red)
-                ),
-                holidayPredicate: (day) => (day.day == 31 && day.month == 12),
-                availableGestures: AvailableGestures.all,
-                selectedDayPredicate: (day) => isSameDay(day, today),
-                onDaySelected: _onDaySelected,
-                firstDay: DateTime(2010,12,12),
-                lastDay: DateTime(2023,12,12),
-              )
-            ],
-          )),
+                  holidayTextStyle: TextStyle(color: Colors.red)),
+              holidayPredicate: (day) => (day.day == 31 && day.month == 12),
+              availableGestures: AvailableGestures.all,
+              selectedDayPredicate: (day) => isSameDay(day, today),
+              onDaySelected: _onDaySelected,
+              firstDay: DateTime(2010, 12, 12),
+              lastDay: DateTime(2023, 12, 12),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
